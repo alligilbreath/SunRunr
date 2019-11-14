ed0535d1c24d45c65503e2418cb9f32801168c83
alligilbreath <alligilbreath@gmail.com>
2019-11-14 12:29:02 -0700

    Working on Route for Users
    Mostly just fixing smoe spacing, adding comments, and debating whether to put deviceData info in here

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 let User = require("../models/users");
 let Device = require("../models/device");
+let deviceData = require("../models/deviceData");
 let fs = require('fs');
 let bcrypt = require("bcryptjs");
 let jwt = require("jwt-simple");
@@ -9,27 +10,29 @@ let jwt = require("jwt-simple");
 /* Authenticate user */
 var secret = fs.readFileSync(__dirname + '/../../jwtkey').toString();
 
+//Sign in a user
 router.post('/signin', function(req, res, next) {
   User.findOne({email: req.body.email}, function(err, user) {
     if (err) {
-       res.status(401).json({success : false, message : "Can't connect to DB."});         
+       res.status(401).json({success : false, message : "Can't connect to DB."});
     }
     else if(!user) {
-       res.status(401).json({success : false, message : "Email or password invalid."});         
+       res.status(401).json({success : false, message : "Email or password invalid."});
     }
     else {
       bcrypt.compare(req.body.password, user.passwordHash, function(err, valid) {
          if (err) {
-           res.status(401).json({success : false, message : "Error authenticating. Contact support."});         
+           res.status(401).json({success : false, message : "Error authenticating. Contact support."});
          }
          else if(valid) {
             var authToken = jwt.encode({email: req.body.email}, secret);
+            //JSON might want a redirect to loggedIn
             res.status(201).json({success:true, authToken: authToken});
          }
          else {
-            res.status(401).json({success : false, message : "Email or password invalid."});         
+            res.status(401).json({success : false, message : "Email or password invalid."});
          }
-         
+
       });
     }
   });
@@ -37,10 +40,10 @@ router.post('/signin', function(req, res, next) {
 
 /* Register a new user */
 router.post('/register', function(req, res, next) {
-   
+
    bcrypt.hash(req.body.password, 10, function(err, hash) {
       if (err) {
-         res.status(400).json({success : false, message : err.errmsg});         
+         res.status(400).json({success : false, message : err.errmsg});
       }
       else {
         var newUser = new User ({
@@ -48,31 +51,32 @@ router.post('/register', function(req, res, next) {
             fullName: req.body.fullName,
             passwordHash: hash
         });
-        
+
         newUser.save(function(err, user) {
           if (err) {
-             res.status(400).json({success : false, message : err.errmsg});         
+             res.status(400).json({success : false, message : err.errmsg});
           }
           else {
-             res.status(201).json({success : true, message : user.fullName + "has been created"});                      
+             res.status(201).json({success : true, message : user.fullName + "has been created"});
           }
         });
       }
-   });   
+   });
 });
 
+//Get account information including user information and devices
 router.get("/account" , function(req, res) {
    // Check for authentication token in x-auth header
    if (!req.headers["x-auth"]) {
       return res.status(401).json({success: false, message: "No authentication token"});
    }
-   
+
    var authToken = req.headers["x-auth"];
-   
+
    try {
       var decodedToken = jwt.decode(authToken, secret);
       var userStatus = {};
-      
+
       User.findOne({email: decodedToken.email}, function(err, user) {
          if(err) {
             return res.status(400).json({success: false, message: "User does not exist."});
@@ -82,22 +86,22 @@ router.get("/account" , function(req, res) {
             userStatus['email'] = user.email;
             userStatus['fullName'] = user.fullName;
             userStatus['lastAccess'] = user.lastAccess;
-            
+
             // Find devices based on decoded token
 		      Device.find({ userEmail : decodedToken.email}, function(err, devices) {
 			      if (!err) {
 			         // Construct device list
-			         let deviceList = []; 
+			         let deviceList = [];
 			         for (device of devices) {
-				         deviceList.push({ 
+				         deviceList.push({
 				               deviceId: device.deviceId,
 				               apikey: device.apikey,
 				         });
 			         }
 			         userStatus['devices'] = deviceList;
 			      }
-			      
-               return res.status(200).json(userStatus);            
+
+               return res.status(200).json(userStatus);
 		      });
          }
       });
